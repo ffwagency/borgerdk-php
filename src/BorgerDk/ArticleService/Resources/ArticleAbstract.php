693466ecf135d9f8d47887599b4ada72240ff1cd
Jens Beltofte <beltofte@gmail.com>
2016-10-24 19:19:03 -0700

    Fixing code style issue breaking Travis

--- src/BorgerDk/ArticleService/Resources/ArticleAbstract.php
+++ src/BorgerDk/ArticleService/Resources/ArticleAbstract.php
@@ -29,7 +29,8 @@ abstract class ArticleAbstract extends ResourceAbstract
     public function getResultFormatted()
     {
         $items = array();
-        $articles = isset($this->resourceResult->Article) ? $this->resourceResult->Article : array($this->resourceResult);
+        $articles = isset($this->resourceResult->Article) ?
+            $this->resourceResult->Article : array($this->resourceResult);
 
         foreach ($articles as $article) {
             $articleItem = $this->formatSingleArticle($article);
